087f3b376b17e69fb3e192cd306417aaed750874
wojciechloboda <loboda.w28@gmail.com>
2022-06-19 21:48:57 +0200

    few changes

---
+++ cmake-build-debug/CMakeFiles/decorator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decorator.dir/drink.cpp.obj"
+  "CMakeFiles/decorator.dir/factory.cpp.obj"
+  "CMakeFiles/decorator.dir/main.cpp.obj"
+  "decorator.exe"
+  "decorator.exe.manifest"
+  "decorator.pdb"
+  "libdecorator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/decorator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
